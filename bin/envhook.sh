23f12986e96a4bb5c6092f4a345a0e5869c9a9a6
John Neffenger <john@status6.com>
2021-01-27 21:11:03 -0800

    Add the OpenJDK reference manuals to the MANPATH
    Prefix the search path for manual pages with the OpenJDK manuals in
    the Snap package by setting the MANPATH environment variable.

--- bin/envhook.sh
+++ bin/envhook.sh
@@ -12,8 +12,10 @@ java=/jdk/bin/java
     printf "# Source this file for OpenJDK environment variables and aliases\n"
     if [ -x "${root}${SNAP}${java}" ]; then
         printf "export JAVA_HOME=%s/jdk\n" "${root}${SNAP}"
+        printf "export MANPATH=%s/jdk/man:\n" "${root}${SNAP}"
     elif [ -x "${SNAP}${java}" ]; then
         printf "export JAVA_HOME=%s/jdk\n" "${SNAP}"
+        printf "export MANPATH=%s/jdk/man:\n" "${SNAP}"
     fi
     printf "alias java='openjdk.java'\n"
     printf "alias javac='openjdk.javac'\n"
